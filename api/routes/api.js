76f328d1760c5f487c2fde0c5a5dd3d1128ebb94
thibaud.besseau@gmail.com <wNFSetlKk!Vc'EGh9W7GKvQ*cefKnhzQ>
2018-12-14 23:26:21 +0100

    add post and put for the endpoint project

--- api/routes/api.js
+++ api/routes/api.js
@@ -11,9 +11,90 @@ const router = express.Router();
 const authenticated = passport.authenticate('jwt', { session: false });
 const user = [];
 
-router.post('/public', (req, res) => {
+router.get('/public', (req, res) => {
   res.send({ message: 'Hey this is a public message!' });
 
+
+})
+
+router.post('/project', (req, res)  => {
+  const project = [];
+  const { projectName, description, amount, timeEstimated, technologies, deadline} = req.body;
+
+
+
+
+  if(projectName== null || amount<0 || timeEstimated <0 || description== null || technologies == null || deadline == null)
+  {
+    res.sendStatus(422);
+    return
+
+  }
+
+
+  project.push({"projectName":projectName, "amount":amount,"description":description,"timeEstimated":timeEstimated, "technologies":technologies,"deadline":deadline});
+
+  mongoClient.connect(dbOptions.dbUrl, function(err, db) {
+
+    if (err) throw err;
+    var dbo = db.db(dbOptions.dbName);
+    dbo.collection("Projects").insert(project, function(err, res) {
+      // if error send code 500
+      if (err)
+      {
+        res.sendStatus(500);
+        throw err;
+
+      }
+
+      // if the user has been added successfully in the DB send code 200
+      db.close();
+    });
+
+  });
+  res.sendStatus(200);
+
+});
+
+router.put('/project/:id', (req, res)  => {
+  const project = [];
+
+  mongoClient.connect(dbOptions.dbUrl, function(err, db) {
+
+    if (err) throw err;
+    let dbo = db.db(dbOptions.dbName);
+    let projectData = null;
+
+    var o_id = new mongoClient.ObjectID(req.params.id);
+
+
+    //TODO check if date < now
+    if(req.body.hasOwnProperty("amount") && req.body.amount <0)
+    {
+      res.sendStatus(422);
+      return
+    }
+
+    dbo.collection("Projects").findOne({"_id":o_id}).then(function (dataProject) {
+      projectData = dataProject;
+      for(var key in projectData) {
+        if(projectData.hasOwnProperty(key) && req.body.hasOwnProperty(key)){
+          projectData[key]=req.body[key];
+        }
+      }
+
+
+      var newvalues = {
+        $set: projectData
+      }
+
+
+      dbo.collection("Projects").updateOne({"_id":o_id},newvalues);
+
+      res.sendStatus(200);
+
+    });
+  });
 
 })
 
@@ -40,7 +121,7 @@ router.post('/register', (req, res) => {
 
     if (err) throw err;
     var dbo = db.db(dbOptions.dbName);
-    dbo.collection("Users").insert(req.body, function(err, res) {
+    dbo.collection("Users").insert(user, function(err, res) {
       // if error send code 500
       if (err)
       {
